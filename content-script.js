218088628f4868f9c2782714690a867cf932ccba
L3af <L3af@tutanota.com>
2021-07-20 15:21:58 +1200

    bug: fix toggle

--- content-script.js
+++ content-script.js
@@ -1,49 +1,61 @@
-function copyStringToClipboard (str) {
-   // Create new element
-   var el = document.createElement('textarea');
-   // Set value (string to be copied)
-   el.value = str;
-   // Set non-editable to avoid focus and move outside of view
-   el.setAttribute('readonly', '');
-   el.style = {position: 'absolute', left: '-9999px'};
-   document.body.appendChild(el);
-   // Select text inside element
-   el.select();
-   // Copy text to clipboard
-   document.execCommand('copy');
-   // Remove temporary element
-   document.body.removeChild(el);
+let copyStringToClipboard = str => {
+  // Create new element
+  var el = document.createElement('textarea');
+  
+  // Set value (string to be copied)
+  el.value = str;
+
+  // Set non-editable to avoid focus and move outside of view
+  el.setAttribute('readonly', '');
+  el.style = { position: 'absolute', left: '-9999px' };
+  document.body.appendChild(el);
+
+  // Select text inside element
+  el.select();
+
+  // Copy text to clipboard
+  document.execCommand('copy');
+
+  // Remove temporary element
+  document.body.removeChild(el);
 }
+
 // Create elements to wrap sentences
-$('p').each(function() {
-      $(this).html($(this).text().split(/([\.\?!])(?= )/).map(
-      function(v){return '<span class=sentence>'+v+'</span>'}
-   ));
+$('p').each(() => {
+  $(this).html(
+    $(this).text()
+      .split(/([\.\?!])(?= )/)
+      .map(v => '<span class=sentence>' + v + '</span>')
+  );
 });
 
 /*
 We're putting a simple json object with "word" and "sentence" attributes to the clipboard
 which will be parsed by ssmtool
 */
-$('.sentence').click(function(){
-   var s = window.getSelection();
-   s.modify('extend','backward','word');        
-   var b = s.toString();
-   
-   s.modify('extend','forward','word');
-   while (s.toString().slice(-1) == "-"){
-      s.modify('extend','forward','word');
-   }
-   a = s.toString();
-   
-   s.modify('move','forward','character');
-   word = (b+a).replace(/[.,\/#!$%\^&\*;:{}=\_`~()]/g, "")
-   console.log(word)
+$('.sentence').click(() => {
+  chrome.storage.sync.get(['enableSSM'], res => {
+    if (!res.enableSSM) return;
 
-   copyobj = {
+    let selection = window.getSelection();
+    selection.modify('extend','backward','word');        
+    let a = selection.toString();
+          
+    selection.modify('extend','forward','word');
+    while (selection.toString().slice(-1) == "-") {
+       selection.modify('extend','forward','word');
+    }
+    let b = selection.toString();
+  
+    selection.modify('move','forward','character');
+    word = (b + a).replace(/[.,\/#!$%\^&\*;:{}=\_`~()]/g, "");
+    console.log(word);
+
+    copyobj = {
       "sentence": $(this).text(),
       "word": word
-   }
+    };
 
-   copyStringToClipboard(JSON.stringify(copyobj));
+    copyStringToClipboard(JSON.stringify(copyobj));
+  });
 });
